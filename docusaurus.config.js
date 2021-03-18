2495636b1dbdb4f6dee962a621116bbe62e1a090
Lokesh Kumar <lokesh.s@leaplogix.in>
2021-03-18 09:58:02 +0530

    fixed url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   title: 'Simply5 CloudLAN',
   tagline: "Democratizing Networking for SMB's",
-  url: 'wiki.simply5.io',
+  url: 'https://wiki.simply5.io',
   baseUrl: '/',
   baseUrlIssueBanner: true,
   onBrokenLinks: 'throw',
