a768981d148e9ed78f621b9ae16f0a14822c4e69
LokeshKumarSY <lokesh.s@leaplogix.in>
2021-03-24 11:42:38 +0530

    tawk bug fixed

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -57,19 +57,10 @@ module.exports = {
     },
   },
   scripts: [
-    `(function(){
-      var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-      s1.async=true;
-      s1.src='https://embed.tawk.to/5f327569f87ad20c6d7cda98/default';
-      s1.charset='UTF-8';
-      s1.setAttribute('crossorigin','*');
-      s0.parentNode.insertBefore(s1,s0);
-      })()`,
     {
-      src:
-        'https://embed.tawk.to/5f327569f87ad20c6d7cda98/default',
-      async: true,
-    },
+      src: "https://embed.tawk.to/5f327569f87ad20c6d7cda98/default",
+      async: true
+    }
   ],
   presets: [
     [
