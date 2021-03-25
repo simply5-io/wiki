8d51519fca648787d098b668eb064bfb3fe493d3
LokeshKumarSY <lokesh.s@leaplogix.in>
2021-03-25 15:57:54 +0530

    Updated wiki edit url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -71,7 +71,7 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/simply5-io/wiki',
+            'https://github.com/simply5-io/wiki/edit/dev/',
         },
         blog: {
           showReadingTime: true,
